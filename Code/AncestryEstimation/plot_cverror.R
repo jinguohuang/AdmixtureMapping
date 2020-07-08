90c0479fefe057b2028918f84c3f93f965ab9f43
Jinguo Huang <46508668+jinguohuang@users.noreply.github.com>
2020-07-08 02:12:04 -0400

    Update plot_cverror.R

--- Code/AncestryEstimation/plot_cverror.R
+++ Code/AncestryEstimation/plot_cverror.R
@@ -15,6 +15,9 @@ cverror<-read.table(cverror)
 colnames(cverror)<-c("K","cv_error")
 
 # plot cv_error against K
-plt<-ggplot(data=cverror, aes(x=K, y=cv_error, group=1)) + geom_line() + geom_point()+ xlab("K") + ylab("Cross-validation error") + ggtitle(paste0("Cross validation error in ", Name))
+plt<-ggplot(data=cverror, aes(x=K, y=cv_error, group=1)) + 
+geom_line() + geom_point()+ 
+xlab("K") + ylab("Cross-validation error") + 
+ggtitle(paste0("Cross validation error in ", Name))
 ggsave(plt,file=paste0(Name,"_cverror.pdf"))
 
