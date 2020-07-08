986ad5b8955bd925e72eeaf3c2b1420d05e2eb08
Jinguo Huang <jinguohuang2018@gmail.com>
2020-07-08 02:08:42 -0400

    add plot_cverror.R

---
+++ Code/AncestryEstimation/plot_cverror.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+# for plotting crossvalidation error of admixture results
+
+#Usage:
+#	put cverror file in the same folder with this script 
+
+# load file
+args = commandArgs(trailingOnly=TRUE)
+cverror<-args[1]
+
+# get filename 
+Name <- sub("\\_.*", '', cverror)
+library(ggplot2)
+cverror<-read.table(cverror)
+colnames(cverror)<-c("K","cv_error")
+
+# plot cv_error against K
+plt<-ggplot(data=cverror, aes(x=K, y=cv_error, group=1)) + geom_line() + geom_point()+ xlab("K") + ylab("Cross-validation error") + ggtitle(paste0("Cross validation error in ", Name))
+ggsave(plt,file=paste0(Name,"_cverror.pdf"))
+
